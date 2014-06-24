ced3b657809544b5d1804aef7e29d391da61de5f
unknown <p1311669@IUTAINFOPC105.univ-lyon1.fr>
2014-06-24 10:32:00 +0200

    added script to create schemas in database

---
+++ database/create_schema.sql
@@ -0,0 +1,12 @@
+drop table geek if exists cascade;
+
+create table geek (
+	id_geek numeric not null,
+	pseudo varchar(255) not null,
+	sex tinyint not null,
+	gravatar_url varchar(255),
+	interest varchar(255) not null,
+	primary key (id_geek)
+);
+
+create sequence geek_seq start with 1000;
