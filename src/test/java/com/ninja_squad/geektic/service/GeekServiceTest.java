19bb4c3c74d79eef9594560d2c980e3f8bfda862
Sylvain PRUDENT <sylvainprudent@hotmail.com>
2014-06-25 11:27:39 +0200

    added mocked test for incrementConsulted service

---
+++ src/test/java/com/ninja_squad/geektic/service/GeekServiceTest.java
@@ -0,0 +1,36 @@
+package com.ninja_squad.geektic.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.ninja_squad.geektic.dao.GeekDao;
+import com.ninja_squad.geektic.entity.Geek;
+
+public class GeekServiceTest {
+
+	@Mock
+	private GeekDao geekDao;
+
+	@InjectMocks
+	private GeekService geekService;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void incrementConsultedTest() {
+		Geek mockedGeek = new Geek();
+		mockedGeek.setConsulted(0);
+		Mockito.when(geekDao.findById(1L)).thenReturn(mockedGeek);
+		geekService.incrementConsulted(1L);
+		assertEquals(1, geekDao.findById(1L).getConsulted());
+	}
+}
